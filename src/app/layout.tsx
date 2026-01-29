9054cdc3efe017197023abfff76dc6104852dd24
br3mah700 <nyeleti.br3mah@gmail.com>
2026-01-30 00:21:02 +0200

    leadership decision management system (leMS):first push

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { Outfit } from 'next/font/google';
+import './globals.css';
+import "flatpickr/dist/flatpickr.css";
+import { SidebarProvider } from '@/context/SidebarContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+import type { Metadata } from 'next';
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.jpg',
+    shortcut: '/favicon.jpg',
+    apple: '/favicon.jpg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} dark:bg-gray-900`}>
+        <ThemeProvider>
+          <SidebarProvider>{children}</SidebarProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
